4f942ea18ca4876c73628f4a57c4f1b016b63e5c
rolandsidamonidze <rolandsidamonidze@gmail.com>
2021-11-03 21:53:03 +0400

    prepare for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
@@ -12,15 +13,23 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/orders', orderRoutes)
 
 app.get('/api/v1/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}else{
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
